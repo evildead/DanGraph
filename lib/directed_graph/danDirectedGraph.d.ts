5ad3d7a30541db2457c8aa55231d03e0330463aa
evildead <danilo.carrabino@gmail.com>
2019-10-30 10:20:11 +0100

    Add correct lib files

--- lib/directed_graph/danDirectedGraph.d.ts
+++ lib/directed_graph/danDirectedGraph.d.ts
@@ -11,10 +11,16 @@ export declare enum ArcType {
 export declare class DanDirectedGraph<I, D> {
     protected _graph: Map<I, DanNodeAndDirectedArcs<I, D>>;
     constructor();
+    protected _getCopyOfInnerGraph(): Map<I, DanNodeAndDirectedArcs<I, D>>;
     addNode(node: DanNode<I, D>): boolean;
     addNodeToId(idNode: I, nodeToAdd: DanNode<I, D>, arcType: ArcType): boolean;
     addNodeToNode(node: DanNode<I, D>, nodeToAdd: DanNode<I, D>, arcType: ArcType): boolean;
+    removeNode(idNode: I): boolean;
+    isNodeALeaf(idNode: I): boolean;
+    protected _getALeaf(): DanNode<I, D> | undefined;
     toString(): string;
     countNodes(): number;
+    protected _isAcyclic(): boolean;
+    isAcyclic(): boolean;
 }
 export {};
